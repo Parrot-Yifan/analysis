1a97b825a3b14b6fb67b0a597cea1f17222a7563
dnishanthraj <143841087+dnishanthraj@users.noreply.github.com>
2024-03-06 19:58:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,15 @@
 
 import time #Import the time module.
 from URL_Analysis.url_analysis import url_analysis_function  # Import the function from url_analysis.py
-# from stock_prediction import stock_prediction_function # Import the function from stock_prediction.py
+from supabase import (
+    create_client,
+    Client
+)  # Import Supabase client for interacting with the Supabase database
+
+# Makes the connection to the Supabase client.
+SUPABASE_URL = "https://irendefjtnvixqkiehya.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyZW5kZWZqdG52aXhxa2llaHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUwNzcxNTUsImV4cCI6MjAyMDY1MzE1NX0.IaTi8UJP4JCcjP35RUNu2gLE3qd_CzQcHgy2y3UTaew"
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # ============================================================
 # START OF PROGRAM
@@ -26,10 +34,13 @@ def main():
 
         # Call the URL analysis program.
         print("Executing URL analysis.")
-        url_analysis_function()
-
+        authentication = supabase.auth.sign_in_with_password({"email": "tudor0404@gmail.com", "password": "test123"})
+        url_analysis_function(supabase)
+        supabase.auth.sign_out()
         # Wait for 5 minutes before performing URL analysis again.
         time.sleep(5 * 60) 
+    
+
 
 if __name__ == "__main__":
     main()
